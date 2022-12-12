0bd38837c8365f2a6e0776d57414485ad0a9c369
alex <sasha_xarlamov@mail.ru>
2022-12-12 23:18:45 +0300

    fixing

--- stage_06/combiner.c
+++ stage_06/combiner.c
@@ -24,7 +24,8 @@ int get_sec(int seconds) {
   return (int)(seconds%60);
 }
 
-pthread_mutex_t ready_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t light_ready_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t sound_ready_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t who_exited_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t iterations_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t light_time_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -38,7 +39,10 @@ enum{
   LIGHT,
   SOUND,
   NONE
-} who_exited = NONE, who_ready = NONE;
+} who_exited = NONE;
+
+char sound_ready = 0;
+char light_ready = 0;
 
 char history[3][256];
 
@@ -74,7 +78,7 @@ void* light_detect(void *thread_data) {
     struct tm *calendar_time = localtime(&epoch_time);
 
     pthread_mutex_lock(&iterations_mutex);
-    int iterations_local = iterations++;
+    int iterations_local = iterations;
     pthread_mutex_unlock(&iterations_mutex);
 
     char history_local[256] = { '\0' };
@@ -92,9 +96,9 @@ void* light_detect(void *thread_data) {
     strcpy(light_time, light_time_local);
     pthread_mutex_unlock(&light_time_mutex);
 
-    pthread_mutex_lock(&ready_mutex);
-    who_ready = LIGHT;
-    pthread_mutex_unlock(&ready_mutex);
+    pthread_mutex_lock(&light_ready_mutex);
+    light_ready = 1;
+    pthread_mutex_unlock(&light_ready_mutex);
   }
 }
 
@@ -135,20 +139,20 @@ void* sound_detect(void *thread_data) {
     struct tm *calendar_time = localtime(&epoch_time);
 
     pthread_mutex_lock(&iterations_mutex);
-    int iterations_local = iterations++;
+    int iterations_local = iterations;
     pthread_mutex_unlock(&iterations_mutex);
 
     char history_local[256] = { '\0' };
-    sprintf("время измерения хлопка № %d: %s\n", iterations, hms_sound);
+    sprintf(history_local, "время измерения хлопка № %d: %d:%d:%d\n", iterations_local, calendar_time->tm_hour, calendar_time->tm_min, calendar_time->tm_sec);
 
-      pthread_mutex_lock(&iterations_mutex);
-      pthread_mutex_unlock(&iterations_mutex);
-    }
-    pclose(fp);
+    pthread_mutex_lock(&history_mutex);
+    strcpy(history[1], history_local);
+    pthread_mutex_unlock(&history_mutex);
 
     pthread_mutex_lock(&sound_time_mutex);
     strcpy(sound_time, sound_time_local);
     pthread_mutex_unlock(&sound_time_mutex);
+
     pthread_mutex_lock(&sound_ready_mutex);
     sound_ready = 1;
     pthread_mutex_unlock(&sound_ready_mutex);
@@ -158,13 +162,13 @@ void* sound_detect(void *thread_data) {
 void* distance_calculate(void *thread_data) {
   while(1) {
     char ready = 0;
-    if(!pthread_mutex_trylock(&sound_ready_mutex)) {
-      if(!pthread_mutex_trylock(&light_ready_mutex)) {
-        ready = light_ready && sound_ready;
-        pthread_mutex_unlock(&light_ready_mutex);
-      }
-      pthread_mutex_unlock(&sound_ready_mutex);
-    }
+
+    pthread_mutex_lock(&sound_ready_mutex);
+    pthread_mutex_lock(&light_ready_mutex);
+    ready = light_ready && sound_ready;
+    pthread_mutex_unlock(&light_ready_mutex);
+    pthread_mutex_unlock(&sound_ready_mutex);
+
     if(ready) {
       pthread_mutex_lock(&sound_ready_mutex);
       pthread_mutex_lock(&light_ready_mutex);
@@ -172,20 +176,21 @@ void* distance_calculate(void *thread_data) {
       sound_ready = 0;
       pthread_mutex_unlock(&light_ready_mutex);
       pthread_mutex_unlock(&sound_ready_mutex);
+
       pthread_mutex_lock(&light_time_mutex);
       pthread_mutex_lock(&sound_time_mutex);
       int distance_seconds = atoi(sound_time) - atoi(light_time);
       pthread_mutex_unlock(&light_time_mutex);
       pthread_mutex_unlock(&sound_time_mutex);
+
       int distance_meters  = distance_seconds * SOUND_SPEED;
       fflush(stdout);
+
       pthread_mutex_lock(&iterations_mutex);
-      printf("расстояние до грозы № %d: %d\n", iterations, distance_meters);
+      printf("расстояние до грозы № %d: %d\n", iterations++, distance_meters);
       pthread_mutex_unlock(&iterations_mutex);
+
       fflush(stdout);
-      pthread_mutex_lock(&iterations_mutex);
-      iterations++;
-      pthread_mutex_unlock(&iterations_mutex);
     }
   }
 }
@@ -194,15 +199,15 @@ void* user_input(void *thread_data) {
   while(1) {
     char input[256] = {'\0'};
     scanf("%s", input);
-    if(!strcmp(input, "stop")){
+    if(!strncmp(input, "stop", 4)){
       system("sudo pkill -SIGINT light_detect");
       system("sudo pkill -SIGINT light_detect");
       exit(0);
     }
-    if(!strcmp(input, "restart")){
-      pthread_mutex_lock(iterations);
+    if(!strncmp(input, "restart", 7)){
+      pthread_mutex_lock(&iterations_mutex);
       iterations = 1;
-      pthread_mutex_unlock(iterations);
+      pthread_mutex_unlock(&iterations_mutex);
     }
   }
 }
@@ -210,23 +215,23 @@ void* user_input(void *thread_data) {
 int main() {
   pthread_t thread_sound;
   if (pthread_create(&thread_sound, NULL, sound_detect, NULL) != 0) {
-    fprintf(stderr, "error: pthread_create was failed\n");
+    fprintf(stderr, "error: pthread_create was failed %d\n", __LINE__);
     exit(-1);
   }
 
   pthread_t thread_light;
   if (pthread_create(&thread_light, NULL, light_detect, NULL) != 0) {
-    fprintf(stderr, "error: pthread_create was failed\n");
+    fprintf(stderr, "error: pthread_create was failed %d\n", __LINE__);
     exit(-1);
   }
 
   pthread_t thread_distance;
   if (pthread_create(&thread_distance, NULL, distance_calculate, NULL) != 0) {
-    fprintf(stderr, "error: pthread_create was failed\n");
+    fprintf(stderr, "error: pthread_create was failed %d\n", __LINE__);
     exit(-1);
   }
 
-  pthread_detach(thread_distance);
+  pthread_join(thread_distance, NULL);
   pthread_join(thread_sound, NULL);
   pthread_join(thread_light, NULL);
 
@@ -249,11 +254,6 @@ int main() {
   //     break;
   //   }
   // }
-
-  close(light_data);
-  close(sound_data);
-
-  execv("sudo ./k.sh", NULL);
 
   return 0;
 }
